aba9ce67b8644fd9cf061abb46bd0bcd65a60366
Zawad Sharif <gangstersoy@gmail.com>
2020-09-13 23:51:01 +0600

    Beautfy the codegit status!

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sideNav = document.getElementById("sideNav");
 sideNav.style.right = "-250px";
 
 menuBtn.onclick = () => {
-    if(sideNav.style.right == "-250px") 
+    if (sideNav.style.right == "-250px")
         sideNav.style.right = "0"
     else
         sideNav.style.right = "-250px"
